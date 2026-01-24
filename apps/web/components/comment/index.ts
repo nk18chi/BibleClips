1b69486e2ffda12f995dc97ac548502cba2c0cdb
Naoki Mita <naoki.mita18@gmail.com>
2026-01-24 11:16:57 -0800

    feat: add comment components index file

---
+++ apps/web/components/comment/index.ts
@@ -0,0 +1,4 @@
+export { CommentCard } from './comment-card';
+export { CommentForm } from './comment-form';
+export { CommentSection } from './comment-section';
+export { ReportModal } from './report-modal';
